e75e10ff4bd00e2e7c09d61274e010b1dd5d3d3f
Ahmed Al Mudarris <68011108+IbnCode@users.noreply.github.com>
2022-08-09 11:47:04 -0400

    Fixing Again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,16 @@ import React  from 'react';
 const Home = () => {
     const pkgs = [
         {title: 'About me', body: 'I am a computer science student with great work ethic and always striving to learn new skills to put towards my work. I have excellent communication skills. I know and understand a wide variety of computer languages and am able to teach others about them. I am always prepared to learn and take constructive criticism by my peers and co-workers. I have experience in research projects and teaching, where I implemented various computer science skills, including but not limited to algorithm optimization, data structures, testing, documentation writing and image processing.',
-            id: 0,  pic: 'https://drive.google.com/file/d/17kw7aXyUU1TuoZpZBwbgmij6EfegTtUn/view?usp=sharing'
+            id: 0,  pic: 'https://myimages.s3.us-east-2.amazonaws.com/Ahmed.jpeg'
         },
         {title: 'Awards and Extracurriculars', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            id: 1,  pic: 'https://drive.google.com/file/d/1nAcV4lFcvfBNa59pKhxXTJqNd7VFJdB_/view?usp=sharing'
+            id: 1,  pic: 'Project.png'
         },
         {title: 'Education', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            id: 2,  pic: 'https://drive.google.com/file/d/1uZwil6yweqnUR1MRFvM0dLtLzGWxPb21/view?usp=sharing'
+            id: 2,  pic: 'McMaster.jpeg'
         },
         {title: 'Experience', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            id: 3, pic: 'https://drive.google.com/file/d/1MHiTVM8NWyuk0X-gsqraz6r_kqTLj10B/view?usp=sharing'
+            id: 3, pic: 'research.png'
         },
     ]
 
