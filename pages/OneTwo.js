2051da9c8cce7cdb62f298ccc63ebe500ef5a6fc
Ahmed Al Mudarris <68011108+IbnCode@users.noreply.github.com>
2022-08-11 15:47:32 -0400

    testing

--- pages/OneTwo.js
+++ pages/OneTwo.js
@@ -3,19 +3,31 @@ import { useLocation } from "react-router-dom";
 import React  from 'react';
 
 
-
 const OneTwo = ({pkgs}) => {
     let loc = useLocation();
+<<<<<<< Updated upstream:pages/OneTwo.js
     return ( 
 
         
         <div className={loc.state.mody}>
+=======
+    const img = useFetch("s3://myimages/Ahmed.jpeg")
+    return (
+
+        <div className={loc.state == null ? "light" : loc.state.mody}>
+            {
+>>>>>>> Stashed changes:src/OneTwo.js
         <div className="home">
-         {pkgs.map((pkg) =>  (
+         {img && pkgs.map((pkg) =>  (
             <div className={pkg.id == 0 ? 'first': 'none'} key = {pkg.id}>
                     <div   className={pkg.id % 2 == 1 ? 'two': 'one'}  >
+<<<<<<< Updated upstream:pages/OneTwo.js
                     <img src = {pkg.pic} />
                 <h3>
+=======
+                    <img src = {img} />
+                    <h3>
+>>>>>>> Stashed changes:src/OneTwo.js
                     {pkg.title}
                 </h3>
                 <p>
